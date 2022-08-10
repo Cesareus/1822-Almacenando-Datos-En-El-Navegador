35035d498a4d7fdb474e83a4463bc9d7f5e52e18
Cesar Gabriel Rosa <cesarrosa2000@gmail.com>
2022-08-10 19:51:12 -0300

    Finalizacion del proyecto

--- components/addTask.js
+++ components/addTask.js
@@ -10,17 +10,20 @@ const addTask = (evento) => {
    const value = input.value;
    const date = calendar.value;
    const dateFormat = moment(date).format("DD/MM/YYYY");
+   const complete = false;
    
    if(value == "" || date == ""){
     return;
    }
       
    input.value = '';
-   calendar.value = ''; 
+   calendar.value = '';   
    
     const taskObj ={
     value,
-    dateFormat
+    dateFormat,
+    complete,
+    id: uuid.v4()
    };
    list.innerHTML="";
 
@@ -35,24 +38,31 @@ const addTask = (evento) => {
 
 
   
- export const createTask = ({value, dateFormat}) => {  
+ export const createTask = ({value, dateFormat, complete, id}) => {      
   
    const task = document.createElement('li');
         task.classList.add('card');
 
    const taskContent = document.createElement('div');
+   
+   const check = checkComplete(id);
+   if(complete){
+     check.classList.toggle('fas');
+     check.classList.toggle('completeIcon');
+     check.classList.toggle('far');
+   }
 
    const titleTask = document.createElement('span');
         titleTask.classList.add('task');
         titleTask.innerText = value;
-        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(check);
         taskContent.appendChild(titleTask); 
 
    const dateTask =  document.createElement('span');   
         dateTask.innerText = dateFormat; 
         task.appendChild(taskContent);
         task.appendChild(dateTask);
-        task.appendChild(deleteIcon());
+        task.appendChild(deleteIcon(id));
    return task;
  };
 
--- components/checkComplete.js
+++ components/checkComplete.js
@@ -1,15 +1,22 @@
-const checkComplete = () => {
+const checkComplete = (id) => {
   const i = document.createElement('i');
   i.classList.add('far', 'fa-check-square', 'icon');
-  i.addEventListener('click', completeTask);
+  i.addEventListener('click', (event) => completeTask(event, id));
   return i;
 };
 // Immediately invoked function expression IIFE
-const completeTask = (event) => {
+const completeTask = (event, id) => {
   const element = event.target;
   element.classList.toggle('fas');
   element.classList.toggle('completeIcon');
   element.classList.toggle('far');
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  const index = tasks.findIndex(item => item.id == id);
+  //compara el elemento del JSON item.id con el id que recibe la función.
+  tasks[index]["complete"] = !tasks[index]["complete"]  //tasks[index]."complete"
+  /*ubica el elemento complete del objeto dentro del array de objetos tasks y lo modifica
+   por el opuesto cada vez que se hace check*/
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
 export default checkComplete;
